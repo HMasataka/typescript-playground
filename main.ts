e13b606ce25f4bbeb4edf09a730f01762d67fe00
HMasataka <siruba.2050@gmail.com>
2024-04-14 15:54:48 +0900

    Feat object

--- main.ts
+++ main.ts
@@ -1,6 +1,18 @@
 import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
 
 const numberScheme = z.number();
-const u = numberScheme.parse(12);
+const n = numberScheme.parse(12);
 
+const userSchema = z.object({
+  name: z.string(),
+  age: z.number(),
+  tel: z.string(),
+});
+type user = z.infer<typeof userSchema>;
+
+const u: user = { name: "hoge", age: 66, tel: "xxx-xxxx-xxxx" };
+const uu = userSchema.parse(u);
+
+console.log(n);
 console.log(u);
+console.log(uu);
